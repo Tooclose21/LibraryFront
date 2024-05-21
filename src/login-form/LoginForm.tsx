44a74025e739d346a37764a5ae1e6636c3f61c77
Tooclose21 <alasuper.as@gmail.com>
2024-05-21 15:37:39 +0200

    Lista 7

--- src/login-form/LoginForm.tsx
+++ src/login-form/LoginForm.tsx
@@ -1,20 +1,36 @@
-import React, { useCallback, useMemo } from 'react';
+import React, { useEffect, useCallback, useMemo } from 'react';
 import './LoginForm.css';
-import { Button, TextField } from '@mui/material';
+import {
+  Button,
+  TextField,
+  Typography,
+  Container,
+  Paper,
+  Box,
+} from '@mui/material';
 import { Formik } from 'formik';
 import * as yup from 'yup';
-import { Link, useNavigate } from 'react-router-dom';
-import { ClientResponse, LibraryClient } from '../library-client';
+import { useNavigate } from 'react-router-dom';
 import { useApi } from '../ApiProvider';
-import { LoginResponseDto } from '../dto/login.dto';
+import { LibraryClient } from '../library-client';
 
 const LoginForm = () => {
   const navigate = useNavigate();
   const apiClient: LibraryClient = useApi();
+
+  useEffect(() => {
+    // Add class to the body
+    document.body.classList.add('body-login');
+
+    // Remove class when component unmounts
+    return () => {
+      document.body.classList.remove('body-login');
+    };
+  }, []);
+
   const onSubmit = useCallback(
     (values: { username: string; password: string }, formik: any) => {
       apiClient.login(values).then((response) => {
-        //console.log(response);
         if (response.success) {
           navigate('/home');
         } else {
@@ -38,54 +54,69 @@ const LoginForm = () => {
   );
 
   return (
-    <div>
-      <Formik
-        initialValues={{ username: '', password: '' }}
-        onSubmit={onSubmit}
-        validationSchema={validationSchema}
-        validateOnChange
-        validateOnBlur
-      >
-        {(formik: any) => (
-          <form
-            className="login-form"
-            id="signForm"
-            noValidate
-            onSubmit={formik.handleSubmit}
+    <Container maxWidth="xs">
+      <Paper elevation={3}>
+        <Box p={3}>
+          <Typography variant="h4" align="center" gutterBottom>
+            Log in to your library
+          </Typography>
+          <Formik
+            initialValues={{ username: '', password: '' }}
+            onSubmit={onSubmit}
+            validationSchema={validationSchema}
+            validateOnChange
+            validateOnBlur
           >
-            <TextField
-              id="username"
-              label="Username"
-              variant="outlined"
-              name="username"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              error={formik.touched.username && !!formik.errors.username}
-              helperText={formik.touched.username && formik.errors.username}
-            />
-            <TextField
-              id="password"
-              label="Password"
-              variant="outlined"
-              type="password"
-              name="password"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              error={formik.touched.password && !!formik.errors.password}
-              helperText={formik.touched.password && formik.errors.password}
-            />
-            <Button
-              variant="contained"
-              type="submit"
-              form="signForm"
-              disabled={!(formik.isValid && formik.dirty)}
-            >
-              Log-in
-            </Button>
-          </form>
-        )}
-      </Formik>
-    </div>
+            {(formik: any) => (
+              <form
+                className="login-form"
+                id="signForm"
+                noValidate
+                onSubmit={formik.handleSubmit}
+              >
+                <TextField
+                  id="username"
+                  label="Username"
+                  variant="outlined"
+                  name="username"
+                  fullWidth
+                  margin="normal"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  error={formik.touched.username && !!formik.errors.username}
+                  helperText={formik.touched.username && formik.errors.username}
+                />
+                <TextField
+                  id="password"
+                  label="Password"
+                  variant="outlined"
+                  type="password"
+                  name="password"
+                  fullWidth
+                  margin="normal"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  error={formik.touched.password && !!formik.errors.password}
+                  helperText={formik.touched.password && formik.errors.password}
+                />
+                <Box mt={2}>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    type="submit"
+                    fullWidth
+                    disabled={!(formik.isValid && formik.dirty)}
+                  >
+                    Log-in
+                  </Button>
+                </Box>
+              </form>
+            )}
+          </Formik>
+        </Box>
+      </Paper>
+    </Container>
   );
 };
+
 export default LoginForm;
